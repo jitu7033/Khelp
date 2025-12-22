6bff77c80ccf6471500635a84afc5f7a4527cbcc
jitendrakumar-chicmic <jitendra.kumar@chicmicstudios.in>
2025-12-22 19:01:28 +0530

    check

--- backend/Routes/user.route.js
+++ backend/Routes/user.route.js
@@ -5,14 +5,14 @@ import { getAllPricesByCropAndType } from '../Models/user.model.js';
 
 
 const router = express.Router();
+// this is register routes 
 
-router.post("/register",adduser);
+router.post("/register", adduser);
 router.post("/login", loginUser);
 
-router.get("/profile",isLoggedIn,(req,res) => {
-    res.status(200).json({message: "User profile", user: req.user});
+router.get("/profile", isLoggedIn, (req, res) => {
+    res.status(200).json({ message: "User profile", user: req.user });
 });
 
 router.get("/get/price", getAllPricesByCropAndType);
-
 export default router;
